181412b8923574fedc9b9d0517d336f31d647ad4
Rychkov Iurii <irychkov@student.hive.fi>
2025-03-19 12:41:56 +0200

    Removed comments.

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 16:17:27 by irychkov          #+#    #+#             */
-/*   Updated: 2025/03/17 17:52:46 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/03/19 11:51:52 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 PhoneBook :: PhoneBook()
 {
 	contacts_count_ = 0;
-	cout << "Constractor for PhoneBook" << endl;
 }
 
 PhoneBook :: ~PhoneBook()
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 16:17:23 by irychkov          #+#    #+#             */
-/*   Updated: 2025/03/17 17:59:04 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/03/19 11:56:54 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,6 @@ int	main(int argc, char **argv)
 		if (input.compare("ADD\0") == 0)
 		{
 			phonebook.add_contact();
-			cout << "Contact added." << endl;
 		}
 		if (input.compare("SEARCH\0") == 0)
 		{
@@ -45,21 +44,19 @@ int	main(int argc, char **argv)
 			}
 			else
 			{
-				cout << "We search. Here we have to show phonebook" << endl;
 				phonebook.search_contact();
-				//Show short here!
-				cout << "Enter index. The program only accepts numbers from 1 to 8 or EXIT" << endl;
+				if (!cin.eof())
+					cout << "Enter index. The program only accepts numbers from 1 to 8 or EXIT to main menu" << endl;
 				while (std::getline(cin, input))
 				{
 					if (input.compare("EXIT\0") == 0)
 					{
-						cout << "We exit" << endl;
+						cout << "We exiting to main menu..." << endl;
 						break;
 					}
 					if (input.length() == 1 && input[0] >= '1' && input[0] <= '8')
 					{
 						int index = stoi(input);
-						cout << "We entered and will show specific contact" << index << endl;
 						if (index > 0 && index <= phonebook.get_contact_count())
 						{
 							phonebook.get_contact(index - 1).display_contact(index);
@@ -70,13 +67,16 @@ int	main(int argc, char **argv)
 						}
 					}
 					if (!cin.eof())
-						cout << "Enter index. The program only accepts numbers from 1 to 8 or EXIT" << endl;
+						cout << "Enter index. The program only accepts numbers from 1 to 8 or EXIT to main menu" << endl;
 				}
 			}
 		}
 		if (!cin.eof())
 			cout << "Enter one of three commands. The program only accepts ADD, SEARCH and EXIT." << endl;
 	}
-	cout << "Thanks! See you!" << endl;
+	if (!cin.eof())
+		cout << "Thanks! See you!" << endl;
+	else
+		cout << "\n\nAt least you've tried! See you! xD" << endl;
 	return (0);
 }
