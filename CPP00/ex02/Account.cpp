f96e50c7943b46ef854c485f4e72e87649bb1e3e
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-03-19 19:52:55 +0200

    Added prototypes to Account.cpp.

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 19:28:17 by irychkov          #+#    #+#             */
-/*   Updated: 2025/03/19 19:43:31 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:52:10 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,21 @@ int	Account  ::  getNbWithdrawals( void )
 	return (_totalNbWithdrawals);
 }
 
+void	Account  ::  _displayTimestamp( void )
+{
+	// here we need to implement the function that will display the current time
+	// for now we will just print the string "timestamp"
+	std::cout << "timestamp";
+}
+
 void	Account  ::  displayAccountsInfos( void )
 {
 	_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount() << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account  ::  displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
