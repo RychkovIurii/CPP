af36e1300f805fc8e470d1605a9bbc9d0c5b7f59
Rychkov Iurii <irychkov@student.hive.fi>
2025-03-20 17:01:08 +0200

    Switched to C++11.

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 11:54:36 by irychkov          #+#    #+#             */
-/*   Updated: 2025/03/20 12:48:30 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/03/20 16:59:05 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,25 @@
 	return (zombie);
 } */
 
-//In the next line we are trying to access the instance of the object that was destroyed
-	//Zombie *zombie2 = newStackZombie("Stack guy");
-	//zombie2->announce();
+/* In the next line we are trying to access the instance
+of the object that was destroyed */
+	/* Zombie *zombie2 = newStackZombie("Stack guy");
+	zombie2->announce(); */
 
-int main()
+int	main()
 {
-	Zombie *zombie;
-
-	zombie = newZombie("Allocated guy");
-	if (zombie == NULL)
+	Zombie*	zombie = nullptr;
+	try
+	{
+		zombie = newZombie("Allocated guy");
+		zombie->announce();
+		delete zombie;
+	}
+	catch (const std::bad_alloc&)
 	{
 		std::cout << "Memory allocation failed" << std::endl;
 		return (1);
 	}
-	zombie->announce();
-	delete zombie;
 	randomChump("Random stack guy");
 	return (0);
 }
