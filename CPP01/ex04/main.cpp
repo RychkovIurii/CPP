971667d9f84e215e84bd5a452a8c66f7261433fb
Rychkov Iurii <irychkov@student.hive.fi>
2025-03-21 17:45:21 +0200

    Draft for ex04(replace).

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 12:19:17 by irychkov          #+#    #+#             */
+/*   Updated: 2025/03/21 17:45:14 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Usage: ./replace filename s1 s2" << std::endl;
+		return (1);
+	}
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::ifstream file(filename);
+
+}
+	
