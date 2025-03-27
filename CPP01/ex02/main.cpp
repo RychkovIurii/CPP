92ec6dbf76178af513c90ae4044e1ee1f6019827
Rychkov Iurii <irychkov@student.hive.fi>
2025-03-27 14:24:55 +0200

    Fixed message.

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 17:06:23 by irychkov          #+#    #+#             */
-/*   Updated: 2025/03/20 17:49:02 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/03/27 14:23:27 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	main( void )
 	std::string	&stringREF = str;
 
 	std::cout << "Address in memory of the string: " << &str << std::endl;
-	std::cout << "Address in memory of the stringPTR: " << stringPTR << std::endl;
-	std::cout << "Address in memory of the stringREF: " << &stringREF << std::endl;
+	std::cout << "Address in memory of the string by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address in memory of the string by stringREF: " << &stringREF << std::endl;
 	std::cout << "String: " << str << std::endl;
 	std::cout << "StringPTR: " << *stringPTR << std::endl;
 	std::cout << "StringREF: " << stringREF << std::endl;
