82d29e1985199e3df06731a36beaad7ec21ef9bb
Rychkov Iurii <irychkov@student.hive.fi>
2025-03-27 16:11:01 +0200

    Added overload operators.

---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 08:58:11 by irychkov          #+#    #+#             */
+/*   Updated: 2025/03/27 16:09:57 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int					_fixed_point_value;
+		static const int	_fractional_bits = 8;
+	public:
+		Fixed( void );
+		Fixed( const int value );
+		Fixed( const float value );
+		Fixed( const Fixed &obj );
+		~Fixed( void );
+		Fixed &operator=(const Fixed &obj);
+		Fixed &operator>(const Fixed &obj);
+		Fixed &operator<(const Fixed &obj);
+		Fixed &operator>=(const Fixed &obj);
+		Fixed &operator<=(const Fixed &obj);
+		Fixed &operator==(const Fixed &obj);
+		Fixed &operator!=(const Fixed &obj);
+		Fixed &operator+(const Fixed &obj);
+		Fixed &operator-(const Fixed &obj);
+		Fixed &operator*(const Fixed &obj);
+		Fixed &operator/(const Fixed &obj);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+		static Fixed &min(Fixed &a, Fixed &b);
+		static Fixed &max(Fixed &a, Fixed &b);
+		static const Fixed &min(const Fixed &a, const Fixed &b);
+		static const Fixed &max(const Fixed &a, const Fixed &b);
+};
+
+std::ostream &operator<<(std::ostream &out, Fixed const &obj);
---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 08:58:14 by irychkov          #+#    #+#             */
+/*   Updated: 2025/03/27 15:46:53 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/*
+Our program handle input use 8 bits for fractional part.
+Min value is -8388608, max value is 8388607.
+*/
+int	main( void )
+{
+	Fixed		a;
+	Fixed const	b( 10 );
+	Fixed const	c( 42.42f );
+	Fixed const	d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return (0);
+}
