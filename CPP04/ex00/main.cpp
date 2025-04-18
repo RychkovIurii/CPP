83b4015b8e5019eb6cbd727f0ced021cb4408183
Rychkov Iurii <irychkov@student.hive.fi>
2025-04-18 11:20:22 +0300

    Added basic_test for ex00.

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 12:31:59 by irychkov          #+#    #+#             */
-/*   Updated: 2025/04/15 18:14:26 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/04/18 11:18:53 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,31 @@
 #include "WrongCat.hpp"
 #include <exception>
 
-int testCorrectAnimals()
+static void basic_test()
+{
+	Cat kitty;
+	kitty.makeSound();
+	std::cout << std::endl;
+
+	std::cout << "\t\tWe execute: Cat kitty2;"
+		<< std::endl << std::endl;
+	Cat kitty2;
+	std::cout << std::endl;
+
+	std::cout << "\t\tWe execute: kitty = kitty2;"
+		<< std::endl << std::endl;
+	kitty = kitty2;
+	std::cout << std::endl;
+
+	std::cout << "\t\tWe execute: Cat kitty3(kitty);"
+		<< std::endl << std::endl;
+	Cat kitty3(kitty);
+	std::cout << std::endl;
+
+	std::cout << "\t\tWe see destructors" << std::endl << std::endl;
+}
+
+static int testCorrectAnimals()
 {
 	int result;
 	const Animal* meta = nullptr;
@@ -49,7 +73,7 @@ int testCorrectAnimals()
 	return (result);
 }
 
-int testWrongAnimals()
+static int testWrongAnimals()
 {
 	int result;
 	const WrongAnimal* meta_wrong = nullptr;
@@ -83,12 +107,11 @@ int	main( void )
 	int test1;
 	int test2;
 
+	basic_test();
+	std::cout << std::endl;
 	test1 = testCorrectAnimals();
 	test2 = testWrongAnimals();
 	if (test1 > test2)
 		return (test1);
 	return (test2);
 }
-
-
-//TEST FOR BASIC OPERATIONS
