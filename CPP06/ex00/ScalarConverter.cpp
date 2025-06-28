123cc573da2242594ae7fd78d81fcd804940d189
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-06-28 17:01:03 +0200

    Added draft for pseudo-literals.

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,51 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 10:42:16 by irychkov          #+#    #+#             */
-/*   Updated: 2025/04/25 10:42:17 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/06/28 16:58:25 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+void ScalarConverter::convert(const std::string &input) {
+
+	if (input.empty()) { // Check for empty input and we can check for other invalid inputs
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
+		return;
+	}
+
+	if (input == "nan" || input == "nanf") {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+		return;
+	}
+	
+	if (input == "-inf" || input == "-inff") {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: -inff" << std::endl;
+		std::cout << "double: -inf" << std::endl;
+		return;
+	}
+
+	if (input == "+inf" || input == "+inff") {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: +inff" << std::endl;
+		std::cout << "double: +inf" << std::endl;
+		return;
+	}
+	
+	// Check if the input is a character
+	// If the input is a single character, we can convert it directly
+	// How can we split logic to handle 0 as a number and as a character?
+
+	// Check if the input is a valid integer
+
+	// Here we implement the logic to convert the input string to float and double
+}	
