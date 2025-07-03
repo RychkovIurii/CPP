2161a363611d4e2c764f6092646ce303a31b379b
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-03 17:44:46 +0200

    Norminette. Removed comments.

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 17:24:32 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/03 17:38:57 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/03 17:42:22 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,22 @@
 #include <iostream>
 
 int main( void ) {
+
 	int a = 2;
 	int b = 3;
+
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
 	return 0;
 }
--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -6,19 +6,9 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 17:24:58 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/03 17:38:24 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/03 17:42:59 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* Implement the following function templates:
-• swap: Swaps the values of two given parameters. Does not return anything.
-• min: Compares the two values passed as parameters and returns the smallest one.
-If they are equal, it returns the second one.
-• max: Compares the two values passed as parameters and returns the greatest one.
-If they are equal, it returns the second one.
-These functions can be called with any type of argument. The only requirement is
-that the two arguments must have the same type and must support all the comparison
-operators. */
 
 #pragma once
 
@@ -26,7 +16,6 @@ operators. */
 - class is the original keyword from C++98.
 - typename was introduced later to improve clarity: it makes more sense to say "a type parameter."
 - class remained for backward compatibility. */
-
 
 template <class T>
 void swap(T &a, T &b) {
