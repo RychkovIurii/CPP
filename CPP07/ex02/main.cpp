46c85c0f0f8e4f9948566e2d70bd5cd28bc4073d
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-05 09:39:37 +0200

    Added for Array ex02 provided tests in main.

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,13 +6,14 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 11:34:07 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/04 14:39:03 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/05 09:36:59 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 #include "Array.tpp"
 #include <iostream>
+#define MAX_VAL 750
 
 /* Notes
 For primitives, new T[n] does nothing — leaves memory uninitialized.
@@ -21,6 +22,7 @@ For classes, the default constructor is called anyway in both cases — so () ha
 */
 
 int main( void ) {
+	// My tests
 	try {
 		Array<int> testInt;
 		Array<std::string> testString;
@@ -102,6 +104,59 @@ int main( void ) {
 		std::cout << "==== Test const array ====" << std::endl;
 		const Array<int> constArray(3);
 		std::cout << "Const array[0]: " << constArray[0] << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+		return 1;
+	}
+
+	// Original tests
+	try {
+		std::cout << "==== Test provided main ====" << std::endl;
+		Array<int> numbers(MAX_VAL);
+		int* mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;//
 	} catch (const std::exception &e) {
 		std::cerr << "Exception: " << e.what() << std::endl;
 		return 1;
