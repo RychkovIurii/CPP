fd40b8e2e0d94946b861f9a6dd95328d0c295d52
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-05 11:13:13 +0200

    Added main with tests for Span.

---
+++ CPP08/ex01/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 10:24:30 by irychkov          #+#    #+#             */
+/*   Updated: 2025/07/05 11:10:48 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+#include <iostream>
+
+int main() {
+	try {
+		std::cout << "==== Valid Span Example:" << std::endl;
+		Span span(5);
+		span.addNumber(1);
+		span.addNumber(5);
+		span.addNumber(2);
+		span.addNumber(10);
+		span.addNumber(8);
+
+		std::cout << "Shortest span: " << span.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << span.longestSpan() << std::endl;
+
+		std::cout << "==== Exception Handling Example:" << std::endl;
+		try {
+			span.addNumber(7); 
+		} catch (const std::overflow_error& e) {
+			std::cerr << "Exception: " << e.what() << std::endl;
+		}
+		try {
+			Span emptySpan(0);
+		} catch (const std::invalid_argument& e) {
+			std::cerr << "Exception: " << e.what() << std::endl;
+		}
+		try {
+			Span singleElementSpan(1);
+			singleElementSpan.addNumber(42);
+			singleElementSpan.shortestSpan();
+		} catch (const std::logic_error& e) {
+			std::cerr << "Exception: " << e.what() << std::endl;
+		}
+		try {
+			Span singleElementSpan(1);
+			singleElementSpan.addNumber(42);
+			singleElementSpan.longestSpan();
+		} catch (const std::logic_error& e) {
+			std::cerr << "Exception: " << e.what() << std::endl;
+		}
+		try {
+			Span singleElementSpan(1);
+			singleElementSpan.addNumber(42);
+			singleElementSpan.addNumber(42);
+		} catch (const std::overflow_error& e) {
+			std::cerr << "Exception: " << e.what() << std::endl;
+		}
+	} catch (const std::exception& e) {
+		std::cerr << "Unexpected exception: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
