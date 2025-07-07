ef62c491eced12081903e2ab220b017d799bb97b
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-07 11:35:19 +0200

    Added addManyNumbers.

--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,11 +6,15 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 10:24:40 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/05 11:06:21 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/07 11:34:15 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <stdexcept>
+#include <limits>
+#include <algorithm>
+#include <iterator> 
 
 class Span {
 	private:
@@ -26,4 +30,22 @@ class Span {
 		void addNumber(int number);
 		unsigned int shortestSpan() const;
 		unsigned int longestSpan() const;
+
+		template <typename Iterator>
+		void addManyNumbers(Iterator begin, Iterator end) {
+			auto dist = std::distance(begin, end);
+			if (dist <= 0) {
+				throw std::invalid_argument("Invalid iterator range: end is before or equal to begin");
+			}
+			unsigned int numbersToAdd = static_cast<unsigned int>(dist);
+			if (std::numeric_limits<unsigned int>::max() - _count < numbersToAdd) {
+				throw std::overflow_error("Too many numbers to add");
+			}
+			if (_count + numbersToAdd > _size) {
+				throw std::overflow_error("Not enough space to add all numbers");
+			}
+			for (Iterator it = begin; it != end; ++it) {
+				addNumber(*it);
+			}
+		}
 };
