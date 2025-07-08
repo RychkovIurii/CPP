a1863a08056b77969410635725a2d46fbcaf23d8
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-08 09:27:06 +0200

    Added draft for loadDB and getExchangeRate.

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,33 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 15:06:02 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/07 15:06:02 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/08 09:22:39 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+#include <fstream>
+
+BitcoinExchange::BitcoinExchange(const std::string &dbFilename) {
+	loadDB(dbFilename);
+}
+
+void BitcoinExchange::loadDB(const std::string &dbFilename) {
+	std::ifstream dbFile(dbFilename);
+	if (!dbFile.is_open()) {
+		throw std::runtime_error("Could not open database file: " + dbFilename);
+	}
+	// Read db and populate exchangeRates
+}
+
+float BitcoinExchange::getExchangeRate(const std::string &date) const {
+	auto it = exchangeRates.lower_bound(date);
+	if (it != exchangeRates.end() && it->first == date) {
+		return it->second;
+	}
+	if (it == exchangeRates.begin()) {
+		throw std::out_of_range("No exchange rate available for date: " + date);
+	}
+	--it;
+	return it->second;
+}
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,23 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 15:05:58 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/07 15:05:59 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/08 08:53:12 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#include <map>
+#include <string>
+
+class BitcoinExchange {
+	public:
+		BitcoinExchange() = delete;
+		BitcoinExchange(const std::string &dbFilename);
+		BitcoinExchange(const BitcoinExchange &other) = delete;
+		BitcoinExchange &operator=(const BitcoinExchange &other) = delete;
+		~BitcoinExchange() = default;
+		float getExchangeRate(const std::string &date) const;
+	private:
+		std::map<std::string, float> exchangeRates;
+		void loadDB(const std::string &dbFilename);
+};
