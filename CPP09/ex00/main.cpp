902de3557d2bde7b5d810a94cae0bf767c38ad9f
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-08 09:27:42 +0200

    Modified main according to the subject.

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 15:05:33 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/07 15:09:20 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/08 09:25:51 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,17 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cout << "Usage: " << argv[0] << " <input_file>" << std::endl;
-		return 0;
+		(void)argv;
+		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
+	}
+
+	try {
+		BitcoinExchange exchange("data.csv");
+		// Read input file and process dates
+	} catch (const std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
 	}
 	return 0;
 }
