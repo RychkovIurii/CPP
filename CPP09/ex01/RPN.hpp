19864b97dd0627f42fce216593a64dbfc238e2a2
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-10 11:40:02 +0200

    Added main and RPN.hpp.

--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -6,8 +6,19 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 20:28:30 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/09 20:28:35 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/10 11:36:26 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <string>
+
+class RPN {
+public:
+	RPN() = delete;
+	RPN(const RPN &other) = delete;
+	RPN &operator=(const RPN &other) = delete;
+	~RPN() = default;
+
+	static void processInput(const std::string &expression);
+};
--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -6,7 +6,26 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 20:29:03 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/09 20:29:04 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/10 11:36:37 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "RPN.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage: " << argv[0] << " <expression>" << std::endl;
+		return 0;
+	}
+
+	try {
+		RPN::processInput(argv[1]);
+	} catch (const std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
