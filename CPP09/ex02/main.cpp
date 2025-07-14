46368925ce024547d13bde4b2079d87803a0a66b
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-14 11:47:37 +0200

    Added stoi, created two containers.  Prints container before sorting.  Used deque and vector.

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 11:08:51 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/11 11:14:05 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/14 11:46:08 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,24 @@ int main(int argc, char **argv)
 	try
 	{
 		// Main logic to process the input numbers
+		std::vector<int> input;
+		for (int i = 1; i < argc; ++i) {
+			int num = std::stoi(argv[i]);
+			if (num < 0) {
+				throw std::invalid_argument("Negative numbers are not allowed.");
+			}
+			input.push_back(num);
+		}
+		std::vector<int> vec(input);
+		std::deque<int> deq(input.begin(), input.end());
+
+		std::cout << "Before: ";
+		for (size_t i = 0; i < input.size(); ++i) {
+			if (i != 0)
+				std::cout << " ";
+			std::cout << input[i];
+		}
+		std::cout << std::endl;
 	} catch (const std::exception &e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
