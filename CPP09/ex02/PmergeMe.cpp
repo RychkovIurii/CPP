cf09e1efadd99775e0dc3eba75501cdc6ae31e78
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-14 12:13:59 +0200

    Added Pmerge class and prototypes.

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,21 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 11:08:40 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/11 11:08:41 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/14 12:13:26 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+#include <algorithm>
+#include <iostream>
+
+void PmergeMe::sortVector(std::vector<int> &vec) {
+	(void)vec; // Placeholder for actual sorting logic
+	std::cout << "Sorting vector..." << std::endl;
+}
+
+void PmergeMe::sortDeque(std::deque<int> &deq) {
+	(void)deq; // Placeholder for actual sorting logic
+	std::cout << "Sorting deque..." << std::endl;
+}
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,8 +6,21 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 11:08:32 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/11 11:14:02 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/14 11:34:42 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <vector>
+#include <deque>
+
+class PmergeMe {
+	public:
+		PmergeMe() = delete;
+		PmergeMe(const PmergeMe &other) = delete;
+		PmergeMe &operator=(const PmergeMe &other) = delete;
+		~PmergeMe() = default;
+
+		static void sortVector(std::vector<int> &vec);
+		static void sortDeque(std::deque<int> &deq);
+};
