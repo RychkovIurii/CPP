990c066e13f2732484c93b8c928240ae44e98fb7
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-07-14 12:13:10 +0200

    Added timer and output messages.

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -6,13 +6,15 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 11:08:51 by irychkov          #+#    #+#             */
-/*   Updated: 2025/07/14 11:46:08 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/07/14 12:11:28 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 #include <iostream>
 #include <stdexcept>
+#include <chrono>
+#include <iomanip>
 
 int main(int argc, char **argv)
 {
@@ -32,8 +34,6 @@ int main(int argc, char **argv)
 			}
 			input.push_back(num);
 		}
-		std::vector<int> vec(input);
-		std::deque<int> deq(input.begin(), input.end());
 
 		std::cout << "Before: ";
 		for (size_t i = 0; i < input.size(); ++i) {
@@ -42,6 +42,34 @@ int main(int argc, char **argv)
 			std::cout << input[i];
 		}
 		std::cout << std::endl;
+
+		std::vector<int> vec(input);
+		auto startTimeForVector = std::chrono::high_resolution_clock::now();
+		PmergeMe::sortVector(vec);
+		auto endTimeForVector = std::chrono::high_resolution_clock::now();
+
+		std::deque<int> deq(input.begin(), input.end());
+		auto startTimeForDeque = std::chrono::high_resolution_clock::now();
+		PmergeMe::sortDeque(deq);
+		auto endTimeForDeque = std::chrono::high_resolution_clock::now();
+		
+		std::cout << "After: ";
+		for (size_t i = 0; i < input.size(); ++i) {
+			if (i != 0)
+				std::cout << " ";
+			std::cout << input[i];
+		}
+		std::cout << std::endl;
+
+		std::chrono::duration<double, std::micro> durationForVector = endTimeForVector - startTimeForVector;
+		std::chrono::duration<double, std::micro> durationForDeque = endTimeForDeque - startTimeForDeque;
+
+		std::cout << std::fixed << std::setprecision(5);
+		std::cout << "Time to process a range of " << vec.size()
+				<< " elements with std::vector : " << durationForVector.count() << " us" << std::endl;
+		std::cout << "Time to process a range of " << deq.size()
+				<< " elements with std::deque  : " << durationForDeque.count() << " us" << std::endl;
+			
 	} catch (const std::exception &e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
